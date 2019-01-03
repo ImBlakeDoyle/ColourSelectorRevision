e42e7f19d55796e80204b0a4dfcfab8e6e9f4fff
Blake <blake.doyle@hotmail.com.au>
2019-01-04 09:23:32 +1100

    create basic canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -23,6 +23,13 @@ class Canvas extends Component {
         this.setContext();
     }
 
+    setContext() {
+        this.context = this.canvasRef.current.getContext('2d');
+        this.context.strokeStyle = this.state.hex;
+        this.context.lineJoin = "round";
+        this.context.lineWidth = 3;
+    }
+
     componentDidMount() {
         console.log(this.canvasRef);
     }
@@ -31,8 +38,38 @@ class Canvas extends Component {
         this.setState({ hex });
     }
 
+    onCanvasMouseMove = (event) => {
+        const x = event.nativeEvent.offsetX;
+        const y = event.nativeEvent.offsetY;
+        const coords = this.state.coords;
+        const { height, width } = this.state;
+
+        if (x > 0 && x < width && y > 0 && y < height) {
+            if (coords) {
+                this.context.beginPath();
+                this.context.moveTo(coords[0], coords[1]);
+                this.context.lineTo(x, y);
+                this.context.closePath();
+                this.context.stroke();
+                this.setState({ coords: [x,y]});
+            }
+        } else {
+            this.setState({ coords: null });
+        }
+    }
+
+    onCanvasMouseUp = (event) => {
+        this.setState({ coords: null});
+    }
+
+    onCanvasMouseDown = (event) => {
+        const x = event.nativeEvent.offsetX;
+        const y = event.nativeEvent.offsetY;
+        this.setState({ coords: [x,y]});
+    }
+
     render() {
-        const { hex } = this.state;
+        const { hex, height, width } = this.state;
 
         return (
             <div>
@@ -44,9 +81,12 @@ class Canvas extends Component {
                 </div>
                 <canvas
                     ref={this.canvasRef}
-                    width="400"
-                    height="400"
+                    width={width}
+                    height={height}
                     style={styles}
+                    onMouseMove={this.onCanvasMouseMove}
+                    onMouseDown={this.onCanvasMouseDown}
+                    onMouseUp={this.onCanvasMouseUp}
                 />
             </div>
         );
